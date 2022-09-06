d5f026c49fa1638a7e8cdd1eb7bfcadde13ce8f7
Tom Boyer <boyer.tom21@gmail.com>
2022-09-06 02:11:21 +0200

    navbar + footer

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ const body = document.querySelector("body");
 toogle.addEventListener("click", function () {
   body.classList.toggle("open");
 });
+
+//darkmode
+
+const darkMode = document.getElementById("dark-mode");
+
+darkMode.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
